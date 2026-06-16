73f9a8f9521e602262ce436c054f154b75b2931d
operatorunk <53313938+operatorunk@users.noreply.github.com>
2026-06-16 15:10:53 +0100

    Create report.py

---
+++ report.py
@@ -0,0 +1,37 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+BITBUCKET_URL = "https://bitbucket.tuempresa.com"
+USERNAME = "TU_USUARIO"
+TOKEN = "TU_TOKEN"
+
+
+def get_projects():
+    start = 0
+
+    while True:
+        url = f"{BITBUCKET_URL}/rest/api/1.0/projects?start={start}"
+
+        response = requests.get(
+            url,
+            auth=HTTPBasicAuth(USERNAME, TOKEN),
+            verify=False
+        )
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        for project in data["values"]:
+            print(
+                f'{project["key"]} - {project["name"]}'
+            )
+
+        if data["isLastPage"]:
+            break
+
+        start = data["nextPageStart"]
+
+
+if __name__ == "__main__":
+    get_projects()
